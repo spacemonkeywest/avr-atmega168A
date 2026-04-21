d5d25dbe769f71af3754acd8e130ae601941a38c
Carlos Aldaco Flores <caldaco12@gmail.com>
2026-04-20 18:54:42 -0700

    A simple blinking LED

---
+++ blink/src/main.cpp
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) 
+{
+    DDRB |= (1 << PB5);
+
+    while (1) 
+    {
+        PORTB ^= (1 << PB5);
+        _delay_ms(1000); // Wait for 1 second
+    }
+
+    return 0;
+}
